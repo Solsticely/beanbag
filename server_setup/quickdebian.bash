d61c40d70ecbe54ef8b93418f638b7623b7b5f25
Solsticely <96320174+Solsticely@users.noreply.github.com>
2026-03-29 00:43:42 -0700

    🔨 Add support for testing on x86_64, and implement autosetup

--- server_setup/quickdebian.bash
+++ server_setup/quickdebian.bash
@@ -14,12 +14,47 @@ case "$MACH" in
   aarch64)
     DEBIAN_ARCH='arm64'
     QEMU_MACH='virt'
+    QEMU_CPU='cortex-a53'
+    QEMU_USE_INITRD=1
+    APPEND_CMD='console=ttyAMA0'
+    ;;
+  x86_64)
+    QEMU_USE_INITRD=0
+    DEBIAN_ARCH='amd64'
+    QEMU_MACH='q35'
+    QEMU_CPU='qemu64'
     ;;
   *)
+    QEMU_USE_INITRD=0
     DEBIAN_ARCH="$MACH"
     QEMU_MACH='pc'
+    QEMU_CPU='host'
     ;;
 esac
+
+# Try to detect whether we have KVM available
+QEMU_EMULATOR=tcg
+set +e
+timeout -k .5 .5s qemu-system-"$MACH" -accel kvm -M "$QEMU_MACH" -nographic
+KVM_ERR="$?"
+set -e
+if [ "$KVM_ERR" -eq 124 ] ; then
+  # KVM is available!
+  # QEMU_CPU=host
+  QEMU_EMULATOR=kvm
+fi
+
+declare -a ARGS
+ARGS=(
+  -machine "$QEMU_MACH"
+  -cpu "$QEMU_CPU"
+  -drive "if=none,file=deb.qcow2,format=qcow2,id=hd"
+  -device "virtio-blk-pci,drive=hd"
+  -m 512m
+  -smp 4
+  -nic "user,hostfwd=tcp::$SSH_PORT-:22"
+  -accel "$QEMU_EMULATOR"
+)
 
 if [ ! -e ./deb.qcow2 ] ; then
   if [ -e ./deb.qcow2.orig ] ; then
@@ -27,63 +62,78 @@ if [ ! -e ./deb.qcow2 ] ; then
     cp ./deb.qcow2.orig ./deb.qcow2
   else
     printf '\033[33;1m%s\033[0m\n' "Downloading debian! This may take a while..."
-    curl -L "https://cloud.debian.org/images/cloud/${DEBIAN_VERSION_NAME}/latest/debian-${DEBIAN_VERSION_NO}-genericcloud-${DEBIAN_ARCH}.qcow2" > deb.qcow2
+    curl -L "https://cloud.debian.org/images/cloud/${DEBIAN_VERSION_NAME}/latest/debian-${DEBIAN_VERSION_NO}-nocloud-${DEBIAN_ARCH}.qcow2" > deb.qcow2
+
+    printf '\033[33;1m%s\033[0m\n' "Configuring image! This may take a while..."
+    virt-customize -a deb.qcow2               \
+      --install openssh-server                \
+      --root-password password:root           \
+      --upload ./sshd_config:/etc/ssh/sshd_config
+
     cp ./deb.qcow2 ./deb.qcow2.orig
   fi
 fi
 
-if [ ! -e initrd.img ] || [ ! -e vmlinuz ] ; then
-  rm ./initrd.img ./vmlinuz || true
+# Extract initrd & kernel on platforms that don't have consistent boot
+if [ "$QEMU_USE_INITRD" -eq 1 ] ; then
+  ARGS+=(
+    -kernel vmlinuz
+    -initrd initrd.img
+    -append "root=/dev/vda1 ro $APPEND_CMD"
+  )
+  if [ ! -e initrd.img ] || [ ! -e vmlinuz ] ; then
+    rm ./initrd.img ./vmlinuz || true
 
-  printf '\033[33;1m%s\033[0m\n' "Extracting kernel and boot images! This may take a while..."
-  eval "$(guestfish --listen)"
-  guestfish --remote add-ro deb.qcow2
-  guestfish --remote run
-  guestfish --remote mount /dev/sda1 /
+    printf '\033[33;1m%s\033[0m\n' "Extracting kernel and boot images! This may take a while..."
+    eval "$(guestfish --listen)"
+    guestfish --remote add-ro deb.qcow2
+    guestfish --remotve run
+    guestfish --remote mount /dev/sda1 /
 
-  LS_BOOT="$(guestfish --remote ls /boot)"
-  VMLINUZ_PATH="/boot/$(printf '%s' "$LS_BOOT" | grep vmlinuz- | head -1)"
-  printf '\033[33;1mVMLINUZ path: %s\033[0m\n' "$VMLINUZ_PATH"
-  INITRD_PATH="/boot/$(printf '%s' "$LS_BOOT" | grep initrd.img- | head -1)"
-  printf '\033[33;1mINITRD path: %s\033[0m\n' "$INITRD_PATH"
+    LS_BOOT="$(guestfish --remote ls /boot)"
+    VMLINUZ_PATH="/boot/$(printf '%s' "$LS_BOOT" | grep vmlinuz- | head -1)"
+    printf '\033[33;1mVMLINUZ path: %s\033[0m\n' "$VMLINUZ_PATH"
+    INITRD_PATH="/boot/$(printf '%s' "$LS_BOOT" | grep initrd.img- | head -1)"
+    printf '\033[33;1mINITRD path: %s\033[0m\n' "$INITRD_PATH"
 
-  printf '\033[33;1m%s\033[0m\n' "Downloading VMLINUZ"
-  guestfish --remote download "$VMLINUZ_PATH" ./vmlinuz
-  printf '\033[33;1m%s\033[0m\n' "Downloading INITRD"
-  guestfish --remote download "$INITRD_PATH" ./initrd.img
+    printf '\033[33;1m%s\033[0m\n' "Downloading VMLINUZ"
+    guestfish --remote download "$VMLINUZ_PATH" ./vmlinuz
+    printf '\033[33;1m%s\033[0m\n' "Downloading INITRD"
+    guestfish --remote download "$INITRD_PATH" ./initrd.img
 
-  printf '\033[33;1m%s\033[0m\n' "DONE!"
-  guestfish --remote exit
+    printf '\033[33;1m%s\033[0m\n' "DONE!"
+    guestfish --remote exit
+  fi
 fi
 
+
 # shellcheck disable=SC2016
-printf '\033[33;1mRun `ssh root@localhost -p %d`\nWhen done, run `killall qemu-system-%s`\nIf this is your first run:\n\trun `apt install openssh-server`, then\n\trun `nano /etc/sshd_config` and set `PermitRootLogin yes` & `PasswordAuthentication yes`, then\n\ton the host machine, run `cp deb.qcow2{,.orig}`\033[0m\n' "$SSH_PORT" "$MACH"
+printf '\033[33;1mRun `ssh root@localhost -p %d`\nWhen done, run `killall qemu-system-%s`\033[0m\n' "$SSH_PORT" "$MACH"
 
 # Thanks to https://blachniet.com/posts/create-a-minimal-local-debian-vm-with-qemu/
 # and https://wiki.qemu.org/Documentation/Platforms/ARM
 # and https://wiki.qemu.org/Documentation/Networking#How_to_get_SSH_access_to_a_guest
 # and https://translatedcode.wordpress.com/2017/07/24/installing-debian-on-qemus-64-bit-arm-virt-board/
-# My system is already virtualised and doesn't support nested virtualisation, so I
-# can't use -accel kvm. If you can, turn it on!
+# and https://trainwit.ch/blog/qemu-serial-defaults/
 qemu-system-"$MACH"                                 \
-  -kernel vmlinuz                                   \
-  -initrd initrd.img                                \
-  -append 'root=/dev/vda1'                          \
-  -machine "$QEMU_MACH"                             \
-  -cpu cortex-a53                                   \
-  -drive if=none,file=deb.qcow2,format=qcow2,id=hd  \
-  -device virtio-blk-pci,drive=hd                   \
-  -m 512m                                           \
-  -smp 4                                            \
-  -nic user,hostfwd=tcp::"$SSH_PORT"-:22            \
-  -chardev stdio,id=char0                           \
-  -mon chardev=char0                                \
-  -serial vc                                        \
+  "${ARGS[@]}"                                      \
+  -serial stdio                                     \
+  -display none                                     \
 
-# To use the terminal, replace from -chardev stdio.... onwards with:
-  # -nographic                                        \
-# and maybe
+# To disable ^C from quitting QEMU, replace -serial stdio with:
   # -serial mon:stdio                                 \
 
-# To use KVM, add the following to the end:
-  # -accel kvm                                        \
+# To use the GUI:
+# 1. change -display none or -nographic with one of:
+  # -display gtk                                      \
+  # -display sdl                                      \
+# 2. change -serial stdio with all of:
+  # -serial vc                                        \
+  # -mon chardev=char0                                \
+  # -chardev stdio,id=char0                           \
+
+# To disable the GUI and simplify the serial console output,
+# replace -chardev ..., -mon ..., and -serial ... with:
+  # -nographic                                        \
+
+reset
